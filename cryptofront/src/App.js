c8577d4fffbfb17bf3eb813254b1428208099b5b
milleriishaun <32374007+milleriishaun@users.noreply.github.com>
2019-07-16 10:55:46 -0700

    Intial Commit

---
+++ cryptofront/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6">column number one</div>
+        <div className="col-6">
+          <span>
+            <i className="fa fa-home" />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
