241b9d5f33167cbe5b645227ec92359d7dd6aacd
milleriishaun <32374007+milleriishaun@users.noreply.github.com>
2019-07-17 19:25:32 -0700

    Add product details

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,13 +1,73 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class Details extends Component {
-    render() {
-        return (
-            <div>
-                <h3> hello from details</h3>
-            </div>
-        );
-    }
+import { ButtonContainer } from "./Button";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
+
+export default class Details extends Component {
+	render() {
+		return (
+			<ProductConsumer>
+				{value => {
+					const {
+						id,
+						title,
+						img,
+						price,
+						company,
+						info,
+						inCart,
+					} = value.detailProduct;
+					return (
+						<div className="container py-5">
+							{/* title */}
+							<div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+								<h1>{title}</h1>
+							</div>
+							{/* end title */}
+							{/* product info */}
+							{/* column for the image */}
+							<div className="row">
+								<div className="col-10 mx-auto col-md-6 my-3">
+									{/* product img */}
+									<img src={img} className="img-fluid" alt="product" />
+								</div>
+								<div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+									{/* product text */}
+									<h2>model: {title}</h2>
+									<h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+										made by : <span className="text-uppercase">{company}</span>
+									</h4>
+									<h4 className="text-blue">
+										<strong>
+											price : <span>$</span>
+											{price}
+										</strong>
+									</h4>
+									<p className="text-capitalize font-weight-bold mt-3 mb-0">
+										some info about the product:
+									</p>
+									<p className="text-muted lead">Lorem ipsum</p>
+									{/* buttons */}
+									<div>
+										<Link to="/">
+											<ButtonContainer>back to products</ButtonContainer>
+										</Link>
+										<ButtonContainer
+											disabled={inCart ? true : false}
+											onClick={() => {
+												value.addToCart(id);
+											}}
+										>
+											{inCart ? "in cart" : "add to cart"}
+										</ButtonContainer>
+									</div>
+								</div>
+							</div>
+						</div>
+					);
+				}}
+			</ProductConsumer>
+		);
+	}
 }
-
-export default Details;
--- src/context.js
+++ src/context.js
@@ -1,5 +1,8 @@
-import React, { Component } from 'react'
-import {detailProduct, storeProducts} from './data';
+import React, { Component } from "react";
+import { detailProduct, storeProducts } from "./data";
+
+//Provider
+//Consumer
 
 const ProductContext = React.createContext();
 // Provider(provides all the information for all the application)(we creat the ProductProvider because Provider is not useful
@@ -8,48 +11,78 @@ const ProductContext = React.createContext();
 // Because the ProductContext.Provider will be sitting at the top of the components tree, we want to return all the children.
 
 class ProductProvider extends Component {
-  state = {
-    products: storeProducts,
-    detailProduct: detailProduct
-  }
+	state = {
+		products: [],
+		detailProduct: detailProduct,
+	};
 
-  handleDetail = () => {
-    console.log('hello from detail');
-  }
+	// lastly, make the method run on on load
+	componentDidMount() {
+		this.setProducts();
+	}
 
-  addToCart = () => {
-    console.log('hello from addToCart');
-  }
+	//first, write this method to get nested data for 'this.state.products' array.
+	setProducts = () => {
+		let tempProducts = [];
+		storeProducts.forEach(item => {
+			// IMPORTANT: second, notice that we are copying the values here, not simply referencing like before
+			const singleItem = { ...item };
+			tempProducts = [...tempProducts, singleItem];
+		});
 
-tester = () => {
-  console.log("State products: ", this.state.products[0].inCart);
-  console.log("Data products: ", storeProducts[0].inCart);
+		this.setState(() => {
+			return { products: tempProducts };
+		});
+	};
 
-  const tempProducts= [...this.state.products];
-  tempProducts[0].inCart = true;
-  this.setState(() => {
-    return {products: tempProducts}
-  }, () => {
-    //nondelayed values
-    console.log("State products: ", this.state.products[0].inCart);
-    console.log("Data products: ", storeProducts[0].inCart);
-  })
-}
+	handleDetail = () => {
+		console.log("hello from detail");
+	};
 
-  render() {
-    return (
-      <ProductContext.Provider value={{
-        ...this.state,
-        handleDetail: this.handleDetail,
-        addToCart: this.addToCart
-      }}>
-        <button onClick={this.tester}>test me</button>
-        {this.props.children}
-      </ProductContext.Provider>
-    )
-  }
+	addToCart = id => {
+		console.log("id", id);
+	};
+
+	// test difference between actual data and referenced data; JS uses references for objects stored in variables or arrays.
+	// JS passes these objects as references.
+	// storeProducts items are objects and they are referenced by the storeProducts array(the state one).
+	// This means that we are not viewing correct data. That is why we want to avoid reference. We want actual data.
+	// We actually want fresh data to use, rather than referenced values. We also want the original values saved.
+	// tester = () => {
+	//   console.log("State products: ", this.state.products[0].inCart);
+	//   console.log("Data products: ", storeProducts[0].inCart);
+
+	//   const tempProducts = [...this.state.products];
+	//   tempProducts[0].inCart = true;
+
+	//   this.setState(
+	//     () => {
+	//       return { products: tempProducts };
+	//     },
+	//     () => {
+	//       //nondelayed values
+	//       console.log("State products: ", this.state.products[0].inCart);
+	//       console.log("Data products: ", storeProducts[0].inCart);
+	//     }
+	//   );
+	// };
+
+	render() {
+		return (
+			<ProductContext.Provider
+				value={{
+					...this.state,
+					handleDetail: this.handleDetail,
+					addToCart: this.addToCart,
+				}}
+			>
+				{/* <button onClick={this.tester}>test me</button> */}
+				{this.props.children}
+			</ProductContext.Provider>
+		);
+	}
 }
 
 const ProductConsumer = ProductContext.Consumer;
 
-export {ProductProvider, ProductConsumer};
+export { ProductProvider, ProductConsumer };
