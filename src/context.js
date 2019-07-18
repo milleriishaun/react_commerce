b5a2a6d8150fe7e22b6dd5f0ff5fbecbd51ae9ea
milleriishaun <32374007+milleriishaun@users.noreply.github.com>
2019-07-17 22:38:15 -0700

    Add modals and totals to cart array

--- src/context.js
+++ src/context.js
@@ -14,6 +14,12 @@ class ProductProvider extends Component {
 	state = {
 		products: [],
 		detailProduct: detailProduct,
+		cart: [],
+		modalOpen: false,
+		modalProduct: detailProduct,
+		cartSubTotal: 0,
+		cartTax: 0,
+		cartTotal: 0,
 	};
 
 	// lastly, make the method run on on load
@@ -50,8 +56,60 @@ class ProductProvider extends Component {
 		});
 	};
 
+	// create a temporary products array
 	addToCart = id => {
-		console.log(`hello from add to cart.id is ${id}`);
+		let tempProducts = [...this.state.products];
+		// use index instead of id, so that react will not move the card to a different spot when reloading
+		const index = tempProducts.indexOf(this.getItem(id));
+		const product = tempProducts[index];
+		product.inCart = true;
+		product.count = 1;
+		const price = product.price;
+		product.total = price;
+
+		this.setState(
+			() => {
+				return {
+					products: tempProducts,
+					cart: [...this.state.cart],
+				};
+			},
+			() => {
+				console.log(this.state);
+			}
+		);
+	};
+
+	openModal = id => {
+		const product = this.getItem(id);
+		this.setState(() => {
+			return {
+				modalProduct: product,
+				modalOpen: true,
+			};
+		});
+	};
+
+	closeModal = () => {
+		this.setState(() => {
+			return { modalOpen: false };
+		});
+	};
+
+	increment = id => {
+		console.log("this is the increment method");
+	};
+
+	decrement = id => {
+		console.log("this is the decrement method");
+	};
+
+	removeItem = id => {
+		console.log("item removed");
+	};
+
+	clearCart = () => {
+		console.log("cart was cleared");
 	};
 
 	// test difference between actual data and referenced data; JS uses references for objects stored in variables or arrays.
@@ -85,6 +143,12 @@ class ProductProvider extends Component {
 					...this.state,
 					handleDetail: this.handleDetail,
 					addToCart: this.addToCart,
+					openModal: this.openModal,
+					closeModal: this.closeModal,
+					increment: this.increment,
+					decrement: this.decrement,
+					removeItem: this.removeItem,
+					clearCart: this.clearCart,
 				}}
 			>
 				{/* <button onClick={this.tester}>test me</button> */}
